d815893ed60208018dd5089413158679a96eaa49
Revanth Anil Kumar <133043149+AzxRevanth@users.noreply.github.com>
2025-08-21 16:53:35 +0530

    Delete src directory

--- src/fetcher.ts
+++
@@ -1,7 +0,0 @@
-import type { Event } from "./types.js";
-
-export async function getData(): Promise<Event[]> {
-  const res = await fetch("./events.json");
-  const data = await res.json();
-  return data as Event[];
-}
